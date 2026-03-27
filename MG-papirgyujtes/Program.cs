0660e1d5a03fdabe1ea21bf486ec9c1feb19bd08
MeszarosGergo1 <meszaros.gergo@diak.szbi-pg.hu>
2026-03-27 18:46:45 +0100

    4. feladat

--- MG-papirgyujtes/Program.cs
+++ MG-papirgyujtes/Program.cs
@@ -11,8 +11,6 @@ namespace papirgyujtes
 {
     class Program
     {
-        
-        
         static void Main(string[] args)
         {
             string kapcsolat = "server=localhost;user=root;password=mysql;database=papirgyujtes;";
@@ -20,10 +18,11 @@ namespace papirgyujtes
             {
                 conn.Open();
                 Console.WriteLine("Sikeres kapcsolódás az adatbázishoz!");
-
+                Console.WriteLine();
+                Console.WriteLine();
                 //3. feladat
-               
-                Console.WriteLine("Készítsen lekérdezést, amely megadja, hogy az első osztályos tanulók mikor és mennyi \npapírt adtak le a gyűjtési időszakban! A lekérdezésben a tanuló neve, osztálya, a leadás \nidőpontja és a leadott papírmennyiség jelenjen meg!");
+
+                Console.WriteLine("3. feladat: \nKészítsen lekérdezést, amely megadja, hogy az első osztályos tanulók mikor és mennyi \npapírt adtak le a gyűjtési időszakban! A lekérdezésben a tanuló neve, osztálya, a leadás \nidőpontja és a leadott papírmennyiség jelenjen meg!");
                 Console.WriteLine();
 
                 string parancs = @"
@@ -44,16 +43,42 @@ namespace papirgyujtes
                         Console.WriteLine($"{reader["nev"],-20} | {reader["osztaly"],-10} | {reader["idopont"],-20} | {reader["mennyiseg"],10} |");
                     }
                 }
-
+                Console.WriteLine(new string('-', 71));
                 Console.WriteLine();
+
+
                 //4. feladat
-                
+                Console.WriteLine();
+                Console.WriteLine();
+                Console.WriteLine("4. feladat: \nListázza ki, hogy az átvételre megjelölt napokon átlagosan mennyi papírt adtak le! A \nszámított mező címkéje „napi atlag” legyen!");
+                Console.WriteLine();
+
+                string sql_script = @"
+                SELECT leadasok.idopont, AVG(leadasok.mennyiseg) AS atlag
+                FROM leadasok
+                GROUP BY leadasok.idopont
+                ORDER BY leadasok.idopont ASC;
+                ";
+
+                using (MySqlCommand cmd = new MySqlCommand(sql_script, conn))
+                using (MySqlDataReader reader = cmd.ExecuteReader())
+                {
+                    Console.WriteLine($"| {"Időpont",-30} | {"Napi átlag",-20} |");
+                    Console.WriteLine(new string('-', 57));
+
+                    while (reader.Read())
+                    {
+                        Console.WriteLine($"| {reader["idopont"],-30} | {reader["atlag"],-20} |");
+                    }
+                }
+                Console.WriteLine(new string('-', 57));
+                Console.WriteLine();
                 //5. feladat
-                
+
                 //6. feladat
-                
+
                 //7. feldat
-                
+
             }
         }
     }
